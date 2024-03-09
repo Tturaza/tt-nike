1eea8af5d09cec3c2b3be2823bcf3c9be74d25fa
Tturaza <temoturazashvili9@gmail.com>
2024-03-09 16:51:41 +0400

    work

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { MyServiceService } from 'src/app/service/my-service.service';
 
 @Component({
@@ -18,5 +18,40 @@ export class HomeComponent implements OnInit {
       console.log(this.popularShoes)
     })
   }
+
+  @ViewChildren('popularNow') trendingMovies!: QueryList<ElementRef>;
+  
+  private isMouseDownMap = new Map<HTMLDivElement, boolean>();
+  private startXMap = new Map<HTMLDivElement, number>();
+  private scrollLeftMap = new Map<HTMLDivElement, number>();
+
+  onMouseDown(container: HTMLDivElement, event: MouseEvent) {
+    this.isMouseDownMap.set(container, true);
+    this.startXMap.set(container, event.pageX - container.offsetLeft);
+    this.scrollLeftMap.set(container, container.scrollLeft);
+  }
+
+  onMouseLeave(container: HTMLDivElement) {
+    this.isMouseDownMap.set(container, false);
+  }
+
+  
+  onMouseUp(container: HTMLDivElement) {
+    this.isMouseDownMap.set(container, false);
+  }
+
+  onMouseMove(container: HTMLDivElement, event: MouseEvent) {
+    if (!this.isMouseDownMap.get(container)) return;
+    event.preventDefault();
+    const x = event.pageX - container.offsetLeft;
+    const startX = this.startXMap.get(container);
+    if (startX !== undefined) {
+        const walk = (x - startX) * 2; 
+        const scrollLeft = this.scrollLeftMap.get(container);
+        if (scrollLeft !== undefined) {
+            container.scrollLeft = scrollLeft - walk;
+        }
+    }
+}
 
 }
